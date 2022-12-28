7883f2a519028d304d89b08414e89d0f5aa1b7fe
tomoki737 <tomoki.k2159@gmail.com>
2022-12-28 21:02:05 +0900

    frontend A作成

---
+++ frontend/A/main.js
@@ -0,0 +1,10 @@
+const option = { responseType: "blob" };
+
+window.onload = function () {
+  const button = document.getElementById("btn");
+  button.addEventListener("click", function () {
+    axios.get("https://catfact.ninja/fact").then((response) => {
+      document.getElementById("title").innerHTML = response.data.fact;
+    });
+  });
+};
