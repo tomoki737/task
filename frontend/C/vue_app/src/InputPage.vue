239c1c898a22d16c8458ba1801e16eace7eae103
tomoki737 <tomoki.k2159@gmail.com>
2023-01-01 16:20:42 +0900

    文字を左右への移動する機能作成

--- frontend/C/vue_app/src/InputPage.vue
+++ frontend/C/vue_app/src/InputPage.vue
@@ -1,43 +1,16 @@
 <template>
-  <v-container class="mt-10" style="max-width: 300px">
-    <v-row>
-      <v-col cols="9" class="px-0">
-        <input
-          type="text"
-          style="outline: solid 1px black; height: 30px; width: 230px"
-          v-model="inputText"
-        />
-      </v-col>
-      <v-col cols="3">
-        <v-btn
-          style="background-color: #000080"
-          tile
-          height="30"
-          dark
-          @click="addText"
-          >追加</v-btn
-        >
-      </v-col>
-
-      <div v-for="(text, index) in allText" :key="index">
-        <div
-          class="mx-0 mb-2"
-          style="border: solid 1px; width: 300px; height: 50px"
-        >
-          <span style="font-size: 16px; margin-left: 20px">{{ fruit }}</span>
-        </div>
-      </div>
+  <v-container class="mt-10" style="width: 700px">
+    <v-row class="d-flex justify-space-between">
+      <left-card></left-card>
+      <right-card></right-card>
     </v-row>
   </v-container>
 </template>
 <script>
+import LeftCard from './components/LeftCardComponent.vue'
+import RightCard from './components/RightCardComponent.vue'
 export default {
-  methods: {
-  },
-  computed: {
-    fruit(){
-      return this.$store.state.menu
-    }
-  }
+  components: {LeftCard,RightCard},
+  methods: {},
 };
 </script>
--- frontend/C/vue_app/src/store/index.js
+++ frontend/C/vue_app/src/store/index.js
@@ -1,18 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    menu:"テスト"
-  },
-  getters: {
+    leftCardFruits: ["Apple", "Grape", "StrawBerry", "Cherry", "Plum"],
+    rightCardFruits: ["Wartermelon", "Banana", "Peach"],
   },
   mutations: {
+    moveLeftCardFruit(state) {
+      const leftCardFruits = state.leftCardFruits;
+      const addFruit = leftCardFruits[leftCardFruits.length -1];
+
+      state.leftCardFruits.pop();
+      state.rightCardFruits.push(addFruit);
+    },
+
+    moveRightCardFruit(state) {
+      const rightCardFruits = state.rightCardFruits;
+      const addFruit = rightCardFruits[rightCardFruits.length -1];
+
+      state.rightCardFruits.pop();
+      state.leftCardFruits.push(addFruit);
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+});
